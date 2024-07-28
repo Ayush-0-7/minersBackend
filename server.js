2cb19c3f7f916ba78e49defc7b02e2235ff7d976
Ayush-0-7 <22052720@kiit.ac.in>
2024-07-28 18:55:55 +0530

    done

--- server.js
+++ server.js
@@ -4,14 +4,8 @@ const app = express();
 const PORT = process.env.PORT ;
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-const router = require('./Routes/orderRoute');
+const router = require('./api/orderRoute');
 app.use(cors());
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
 app.use(express.json());
 app.use(cookieParser());
 app.use('/api',router);
